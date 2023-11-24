4d75708035f7e7dd77236b4db650dfd84a3a7d9e
Isachenko Ivan <vanisachenko@gmail.com>
2023-11-25 02:03:45 +0600

    feat: file processing functionality

--- apps/backend/src/process/process.controller.ts
+++ apps/backend/src/process/process.controller.ts
@@ -1,12 +1,31 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ProcessService } from './process.service';
 import { ResultDto } from './dtos/result.dto';
+import { Express } from 'express'
+import { FileInterceptor } from '@nestjs/platform-express';
+import {Multer} from 'multer'
+import * as xlsx from 'node-xlsx'
 
 @Controller('process')
 export class ProcessController {
     constructor(
         private readonly processService: ProcessService
     ){}
+
+
+    @Post('file')
+    @UseInterceptors(FileInterceptor('file'))
+    uploadFile(@UploadedFile() file: Express.Multer.File)
+    {
+        var table = xlsx.parse(file.buffer)
+        Logger.log(table)
+        let response = []
+        for (const text of table[0].data)
+        {
+            response.push(this.processService.processText(text[0]))
+        }
+        return response
+    }
 
     @Post('')
     public async processText(@Body() data: ResultDto)
@@ -20,7 +39,7 @@ export class ProcessController {
     {
         return this.processService.saveRecord(data)
     }
-    
+
     @Post(':id')
     public async editRecord(@Param('id') id: number, @Body() data: ResultDto)
     {
