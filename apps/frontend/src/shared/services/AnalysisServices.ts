f84d3a58abbc4f4269c846a083623d8e6923b13a
romaiy <sokolovgit256@gmail.com>
2023-11-24 15:17:28 +0600

    fix: api url fix

--- apps/frontend/src/shared/services/AnalysisServices.ts
+++ apps/frontend/src/shared/services/AnalysisServices.ts
@@ -4,12 +4,12 @@ import { IResult } from 'shared/models/IResult';
 
 export default class AnalysisServices {
   static async setAnalysis(text?: string): Promise<AxiosResponse<number>> {
-    return $api.post<number>('/analysis', { text });
+    return $api.post<number>('/process', { text });
   }
 
   static async getAnalysisResult(
     id: number
   ): Promise<AxiosResponse<IResult[]>> {
-    return $api.get<IResult[]>(`/analysis/${id}`);
+    return $api.get<IResult[]>(`/process/${id}`);
   }
 }
